20ba83942e8da51c7d747e2391cf147cab1e76ba
Vivek Bisht <vivekbisht4320@gmail.com>
2025-06-21 22:09:30 +0530

    Updated

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,15 +1,12 @@
-const express = require(express);
+const express = require('express');
 const router = express.Router();
-const Blog = require('../models/Blog');
+const {createBlog , getAllBlogs , getBlogsByID ,updateBlog,deleteBlog} = require('../controllers/blogController');
 
-router.post('/', async (req, res) => {
-  try {
-    const blog = new Blog(req.body);
-    const saved = await blog.save();
-    res.status(201).json(saved);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
+router.get('/' , getAllBlogs());
+router.get('/:id' , getBlogsByID());
+router.post('/', createBlog());
+router.get('/:id' , updateBlog());
+router.get('/:id' , deleteBlog());
+
 
 module.exports = router;
